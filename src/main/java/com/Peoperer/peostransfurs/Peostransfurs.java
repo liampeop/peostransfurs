249e3e7352291fe46a6ce12109e08211305c6b2b
liampeop <Peoperer1@gmail.com>
2026-06-04 19:46:44 +0200

    fixes

--- src/main/java/com/Peoperer/peostransfurs/Peostransfurs.java
+++ src/main/java/com/Peoperer/peostransfurs/Peostransfurs.java
@@ -3,10 +3,9 @@ package com.Peoperer.peostransfurs;
 import com.Peoperer.peostransfurs.entity.ModAbilities;
 import com.Peoperer.peostransfurs.entity.ModEntities;
 import com.Peoperer.peostransfurs.entity.ModTransfurVariants;
-import com.Peoperer.peostransfurs.entity.client.Teal_Dragon_Model;
-import com.Peoperer.peostransfurs.entity.client.Teal_Dragon_Renderer;
-import com.Peoperer.peostransfurs.entity.client.White_Mouse_Model;
-import com.Peoperer.peostransfurs.entity.client.White_Mouse_Renderer;
+import com.Peoperer.peostransfurs.entity.client.*;
+import com.Peoperer.peostransfurs.entity.custom.Brown_Mouse;
+import com.Peoperer.peostransfurs.entity.custom.Latex_Mylo_Panda;
 import com.Peoperer.peostransfurs.entity.custom.Teal_Dragon;
 import com.Peoperer.peostransfurs.entity.custom.White_Mouse;
 import com.Peoperer.peostransfurs.item.ModCreativeModeTabs;
@@ -24,8 +23,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-import static com.Peoperer.peostransfurs.entity.ModEntities.TEAL_DRAGON;
-import static com.Peoperer.peostransfurs.entity.ModEntities.WHITE_MOUSE;
+import static com.Peoperer.peostransfurs.entity.ModEntities.*;
 import static net.ltxprogrammer.changed.init.ChangedEntityRenderers.registerHumanoid;
 
 @Mod(Peostransfurs.MODID)
@@ -60,14 +58,18 @@ public class Peostransfurs {
     public static class ModEvents {
         @SubscribeEvent
         public static void registerAttributes(EntityAttributeCreationEvent event) {
-            event.put(ModEntities.TEAL_DRAGON.get(), Teal_Dragon.createAttribures().build());
+            event.put(TEAL_DRAGON.get(), Teal_Dragon.createAttribures().build());
             event.put(WHITE_MOUSE.get(), White_Mouse.createAttribures().build());
+            event.put(BROWN_MOUSE.get(), Brown_Mouse.createAttribures().build());
+            event.put(LATEX_MYLO_PANDA.get(), Latex_Mylo_Panda.createAttribures().build());
         }
 
         @SubscribeEvent
         public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
             event.registerLayerDefinition(Teal_Dragon_Model.LAYER_LOCATION, Teal_Dragon_Model::createBodyLayer);
             event.registerLayerDefinition(White_Mouse_Model.LAYER_LOCATION, White_Mouse_Model::createBodyLayer);
+            event.registerLayerDefinition(Brown_Mouse_Model.LAYER_LOCATION, Brown_Mouse_Model::createBodyLayer);
+            event.registerLayerDefinition(Latex_Mylo_Panda_Model.LAYER_LOCATION, Latex_Mylo_Panda_Model::createBodyLayer);
         }
     }
 
@@ -77,6 +79,8 @@ public class Peostransfurs {
         public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
             registerHumanoid(event, TEAL_DRAGON.get(), Teal_Dragon_Renderer::new);
             registerHumanoid(event, WHITE_MOUSE.get(), White_Mouse_Renderer::new);
+            registerHumanoid(event, BROWN_MOUSE.get(), Brown_Mouse_Renderer::new);
+            registerHumanoid(event, LATEX_MYLO_PANDA.get(), Latex_Mylo_Panda_Renderer::new);
         }
     }
 }
---
+++ src/main/java/com/Peoperer/peostransfurs/entity/client/Latex_Mylo_Panda_Renderer.java
@@ -0,0 +1,28 @@
+package com.Peoperer.peostransfurs.entity.client;
+
+import com.Peoperer.peostransfurs.Peostransfurs;
+import com.Peoperer.peostransfurs.entity.custom.Brown_Mouse;
+import com.Peoperer.peostransfurs.entity.custom.Latex_Mylo_Panda;
+import net.ltxprogrammer.changed.client.renderer.AdvancedHumanoidRenderer;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.GasMaskLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.TransfurCapeLayer;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleCatModel;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+
+public class Latex_Mylo_Panda_Renderer extends AdvancedHumanoidRenderer<Latex_Mylo_Panda, Latex_Mylo_Panda_Model> {
+    private static final ResourceLocation TEXTURE = new ResourceLocation(Peostransfurs.MODID, "textures/latex_mylo_panda.png");
+
+    public Latex_Mylo_Panda_Renderer(EntityRendererProvider.Context context) {
+        super(context, new Latex_Mylo_Panda_Model(context.bakeLayer(Brown_Mouse_Model.LAYER_LOCATION)), ArmorLatexMaleCatModel.MODEL_SET, 0.5f);
+        this.addLayer(TransfurCapeLayer.normalCape(this, context.getModelSet()));
+        this.addLayer(new CustomEyesLayer(this, context.getModelSet()));
+        this.addLayer(GasMaskLayer.forSnouted(this, context.getModelSet()));
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(Latex_Mylo_Panda tealDragon) {
+        return TEXTURE;
+    }
+}
